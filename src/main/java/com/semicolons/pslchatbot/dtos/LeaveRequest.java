be11600e770d79dedcb40ab2cb7a9cd2e1d6740c
Gunnam, Srinivas <c-Srinivas.Gunnam@lifetech.com>
2019-03-09 13:31:54 -0800

    new api

---
+++ src/main/java/com/semicolons/pslchatbot/dtos/LeaveRequest.java
@@ -0,0 +1,30 @@
+package com.semicolons.pslchatbot.dtos;
+
+public class LeaveRequest {
+	
+	private String leaveType;
+	private String startDate;
+	private String endDate;
+	
+	public String getLeaveType() {
+		return leaveType;
+	}
+	public void setLeaveType(String leaveType) {
+		this.leaveType = leaveType;
+	}
+	public String getStartDate() {
+		return startDate;
+	}
+	public void setStartDate(String startDate) {
+		this.startDate = startDate;
+	}
+	public String getEndDate() {
+		return endDate;
+	}
+	public void setEndDate(String endDate) {
+		this.endDate = endDate;
+	}
+	
+	
+
+}
